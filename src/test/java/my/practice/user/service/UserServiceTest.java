5e6cad3b6c75f4ac6a5631a57072c1dd13180324
LeeJeondo <jeondo.lee@kbfg.com>
2024-01-05 17:58:40 +0900

    테스트 추가

---
+++ src/test/java/my/practice/user/service/UserServiceTest.java
@@ -0,0 +1,12 @@
+package my.practice.user.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserServiceTest {
+
+	@Test
+	@DisplayName("유저 생성")
+	void create() {
+	}
+}
