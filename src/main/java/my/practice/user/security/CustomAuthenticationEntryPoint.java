2c995f3be525780d3d792d15e25dae0eec55a4e8
D220540 <Gnlfhfh12@>
2024-01-06 00:01:45 +0900

    Spring Security 설정 추가중

--- src/main/java/my/practice/user/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/my/practice/user/security/CustomAuthenticationEntryPoint.java
@@ -24,6 +24,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.setContentType("application/json;charset=utf-8");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
         String json = objectMapper.writeValueAsString(Map.of("status", HttpStatus.UNAUTHORIZED,
                 "message", "잘못된 접근입니다."));
         PrintWriter out = response.getWriter();
